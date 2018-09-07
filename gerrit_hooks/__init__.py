bb4ea0a08314ac7305d76df0334d78a9bd2cd242
nils.diefenbach <nils.diefenbach@celeraone.com>
2018-09-07 21:00:27 +0200

    Issue-#1 Implement auto-detection of hook type.
    Also, we fixed a cyclic import in our code.

--- gerrit_hooks/__init__.py
+++ gerrit_hooks/__init__.py
@@ -1,4 +1,5 @@
 from gerrit_hooks.containers import SupportedHooks, HookFlagDefinitions
+from gerrit_hooks.hook import parse_options, build_parser_for
 
 HOOKS = SupportedHooks()
 FLAGS = HookFlagDefinitions()
--- gerrit_hooks/hook.py
+++ gerrit_hooks/hook.py
@@ -1,7 +1,9 @@
 """Python3 utility to build Gerrit Hook CLI Arg parsers."""
 import argparse
+import pathlib
+from __main__ import __file__
 
-from gerrit_hooks import HOOKS, FLAGS
+from gerrit_hooks.containers import SupportedHooks, HookFlagDefinitions
 
 
 def _generate_parser(**cli_kwargs):
@@ -22,17 +24,27 @@ def build_parser_for(hook_type):
     :param str hook_type: The hook type string to generate an argument parser from.
     :rtype: argparse.ArgumentParser
     """
-    cli_flags = FLAGS[hook_type]
+    cli_flags = HookFlagDefinitions()[hook_type]
     return _generate_parser(**cli_flags)
 
 
-def parse_options(hook_type):
+def parse_options(hook_type=__file__):
     """Build a parser and parse the CLI options for the given hook type.
 
+    We support passing `__file__`, which may or may not be an absolute path
+    (depending on the platform) - if it is, we use the pathlib module to
+    exctract the hook's name.
+    To do this, we convert ``hook_type`` to a pathlib.Path object and get its
+    pathlib.Path.stem property - which is the last part of the path, sans
+    extension, if any.
+
     :param str hook_type: The type of hook to build and parse options for.
+    :raises ValueError: If hook_type is not found in the SupportedHooks container.
     :rtype: Type[argparse.NameSpace]
     """
-    if hook_type not in HOOKS:
+    hook_type = pathlib.Path(hook_type).stem
+
+    if hook_type not in SupportedHooks():
         raise ValueError("Unknown Hook type: {}".format(hook_type))
     return build_parser_for(hook_type).parse_args()
 
