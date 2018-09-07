4d6153bcb99b537c0c18a4ef120cc850f84002a6
nils.diefenbach <nils.diefenbach@celeraone.com>
2018-09-07 20:43:19 +0200

    Use constants for look ups.

--- gerrit_hooks/hook.py
+++ gerrit_hooks/hook.py
@@ -1,6 +1,8 @@
 """Python3 utility to build Gerrit Hook CLI Arg parsers."""
 import argparse
 
+from gerrit_hooks import HOOKS, FLAGS
+
 
 def _generate_parser(**cli_kwargs):
     """Set up an argument parser with the given expected flags as kwargs.
@@ -20,7 +22,7 @@ def build_parser_for(hook_type):
     :param str hook_type: The hook type string to generate an argument parser from.
     :rtype: argparse.ArgumentParser
     """
-    cli_flags = HookFlagDefinitions.get_flags(hook_type)
+    cli_flags = FLAGS[hook_type]
     return _generate_parser(**cli_flags)
 
 
@@ -30,7 +32,7 @@ def parse_options(hook_type):
     :param str hook_type: The type of hook to build and parse options for.
     :rtype: Type[argparse.NameSpace]
     """
-    if hook_type not in HookType.__all__:
+    if hook_type not in HOOKS:
         raise ValueError("Unknown Hook type: {}".format(hook_type))
     return build_parser_for(hook_type).parse_args()
 
